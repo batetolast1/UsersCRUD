f63991909578c26886aa38e12378c560ae1d365c
batetolast1 <batetolast1@gmail.com>
2020-08-21 22:17:26 +0200

    added utf-8 letter characters support to username regex validation

--- src/main/java/pl/coderslab/jeeusercrud/regex/UserNameValidator.java
+++ src/main/java/pl/coderslab/jeeusercrud/regex/UserNameValidator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class UserNameValidator {
     // Alphanumeric string that may include ".", "_" and "–" having a length of 3 to 16 characters
-    private static final String USERNAME_PATTERN = "^[a-z0-9._-]{3,16}$";
+    private static final String USERNAME_PATTERN = "^[\\p{L}]{3,16}$";
     private static final Pattern PATTERN = Pattern.compile(USERNAME_PATTERN);
 
     public static boolean validate(String username) {
